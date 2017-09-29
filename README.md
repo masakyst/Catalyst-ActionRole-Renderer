d0f015035411a3951d7c947088c19db23a8df9af
Masaaki Saito <masakyst.mobile@gmail.com>
2017-09-29 20:30:38 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Catalyst::ActionRole::Renderer - Blah blah blah
+
+# SYNOPSIS
+
+    use Catalyst::ActionRole::Renderer;
+
+# DESCRIPTION
+
+Catalyst::ActionRole::Renderer is
+
+# AUTHOR
+
+Masaaki Saito <masakyst.public@gmail.com>
+
+# COPYRIGHT
+
+Copyright 2017- Masaaki Saito
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Catalyst/ActionRole/Renderer.pm
@@ -0,0 +1,39 @@
+package Catalyst::ActionRole::Renderer;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Catalyst::ActionRole::Renderer - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Catalyst::ActionRole::Renderer;
+
+=head1 DESCRIPTION
+
+Catalyst::ActionRole::Renderer is
+
+=head1 AUTHOR
+
+Masaaki Saito E<lt>masakyst.public@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2017- Masaaki Saito
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
